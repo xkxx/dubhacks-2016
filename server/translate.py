a2743f4fd435be21a265e7007b3592c6015d55f3
Kexiang Xu <xkxx@attu1.cs.washington.edu>
2016-10-15 23:57:54 -0700

    added transform()

--- server/translate.py
+++ server/translate.py
@@ -17,6 +17,18 @@ pos_map = {
   'VERB': 'verb',
 }
 
+def transform(doc_str):
+  tups = tokenize(doc_str)
+  return [translate(x) if is_hard(x) else x[0] for x in tups]
+  
+def translate(tup):
+  syns = get_syn(tup)
+  if len(syns) == 0:
+    return tup[0]
+  syns_with_scores = [(get_score(syn), syn) for syn in syns]
+  best = min(syns_with_scores)[1]
+  return best
+
 def tokenize(doc_str):
   doc = en_nlp(doc_str)
   return [(x.orth_, x.lemma_, pos_map.get(x.pos_, 'other')) for x in doc]
@@ -35,11 +47,21 @@ def parse_syn(res, pos):
       return item['synonyms']
   return []
   
+def is_hard(tup):
+  if len(tup[0]) < 3:
+    return False
+  return get_score(tup) > 100
+
+def get_score(tup):
+  return 100000
+
 ## UNIT TESTS
 
 def test():
   print tokenize(u"Merry has a little sheep")
   print get_syn(('unhinged', 'unhinged', 'adjective'))
+  print translate(('unhinged', 'unhinged', 'adjective'))
+  print transform(u"Exhausted from reading, I closed my door and got in bed.")
 
 if __name__ == '__main__':
   test()
