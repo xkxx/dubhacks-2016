18ffbfbd85fc589e2d59096e4e020ce19257f7c7
Vaughn Johnson <vneck@D-173-250-158-57.dhcp4.washington.edu>
2016-10-16 01:14:05 -0700

    Comments and additional tests

--- server/translate.py
+++ server/translate.py
@@ -3,10 +3,15 @@ import spacy
 import spacy.parts_of_speech as POS
 import requests
 
+#Contains the relative frequency of all the words in a corpus, with the most frequently used word ranked "1"
+ranks = load_ranks()
+
+#The "reading level", or relative frequency we use to establish whether a word is "difficult"
+relative_hard = 10000
 en_nlp = spacy.load('en')
 word_endpoint = 'https://wordsapiv1.p.mashape.com/words/'
 word_headers = {
-  'X-Mashape-Key': API_KEY, 
+  'X-Mashape-Key': API_KEY,
   'Accept': 'application/json',
 }
 
@@ -20,7 +25,7 @@ pos_map = {
 def transform(doc_str):
   tups = tokenize(doc_str)
   return [translate(x) if is_hard(x) else x[0] for x in tups]
-  
+
 def translate(tup):
   syns = get_syn(tup)
   if len(syns) == 0:
@@ -35,7 +40,7 @@ def tokenize(doc_str):
 
 def get_syn(tup):
   (word, lemma, pos) = tup
-  
+
   res = requests.get(word_endpoint + lemma, headers=word_headers).json()
   syns = parse_syn(res, pos)
   return syns
@@ -46,15 +51,25 @@ def parse_syn(res, pos):
     if item['partOfSpeech'] == pos:
       return item['synonyms']
   return []
-  
+
 def is_hard(tup):
   if len(tup[0]) < 3:
     return False
-  return get_score(tup) > 100
+  return get_score(tup[0]) > relative_hard
 
 def get_score(tup):
-  return 100000
+  return rank[tup[0]]
 
+def load_ranks():
+    corpus = open("word_order.csv").readlines()
+    print corpus[0:10]
+    rank = {}
+
+    for line in corpus:
+        line = line.split(",")
+        ranks[line[0]] = int(line[1][1:-1])
+
+    return rank
 ## UNIT TESTS
 
 def test():
@@ -62,6 +77,9 @@ def test():
   print get_syn(('unhinged', 'unhinged', 'adjective'))
   print translate(('unhinged', 'unhinged', 'adjective'))
   print transform(u"Exhausted from reading, I closed my door and got in bed.")
+  print is_hard((u'transpirating',))
+  print is_hard((u'the',))
+  print get_score((u'the',))
 
 if __name__ == '__main__':
   test()
